20d0a555806b6240f88bf4b48adcf6a00516244a
anw48 <mail@andrewwynnewilliams.co.uk>
2016-02-25 14:38:55 +0000

    My commit

---
+++ app/src/main/java/com/example/andrew/opendayapp/DepartmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.andrew.opendayapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DepartmentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_department);
+    }
+}
--- app/src/main/java/com/example/andrew/opendayapp/MainActivity.java
+++ app/src/main/java/com/example/andrew/opendayapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.andrew.opendayapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+    ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // found online check links
+        button = (ImageButton) findViewById(R.id.welcomeimageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        WelcomeActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
