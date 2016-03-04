921c264fb372a7eeba355313eeafcd3304ff2952
anw48 <mail@andrewwynnewilliams.co.uk>
2016-03-04 15:50:29 +0000

    My commit

---
+++ app/src/main/java/com/example/andrew/opendayapp/BusActivity.java
@@ -0,0 +1,28 @@
+package com.example.andrew.opendayapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class BusActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return false;
+    }
+    }
+
--- app/src/main/java/com/example/andrew/opendayapp/Getting_Around_Activity.java
+++ app/src/main/java/com/example/andrew/opendayapp/Getting_Around_Activity.java
@@ -1,17 +1,35 @@
 package com.example.andrew.opendayapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Getting_Around_Activity extends Activity {
+public class Getting_Around_Activity extends Activity implements View.OnClickListener {
+
+    ImageButton mapbutton, carbutton, trainbutton, busbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_around_);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mapbutton = (ImageButton) findViewById(R.id.mapImageButton);
+        mapbutton.setOnClickListener(this);
+
+        carbutton = (ImageButton) findViewById(R.id.carImageButton);
+        carbutton.setOnClickListener(this);
+
+        trainbutton = (ImageButton) findViewById(R.id.trainImageButton);
+        trainbutton.setOnClickListener(this);
+
+        busbutton = (ImageButton) findViewById(R.id.busImageButton);
+        busbutton.setOnClickListener(this);
+
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -22,5 +40,38 @@ public class Getting_Around_Activity extends Activity {
                 return true;
         }
         return false;
+    }
+
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.mapImageButton: {
+                Intent myIntent = new Intent(Getting_Around_Activity.this,
+                        WelcomeActivity.class);
+                startActivity(myIntent);
+                break;
+            }
+            case R.id.carImageButton: {
+                Intent myIntent = new Intent(Getting_Around_Activity.this,
+                        CarActivity.class);
+                startActivity(myIntent);
+                break;
+            }
+            case R.id.trainImageButton: {
+                Intent myIntent = new Intent(Getting_Around_Activity.this,
+                        TrainActivity.class);
+                startActivity(myIntent);
+                break;
+            }
+            case R.id.busImageButton: {
+                Intent myIntent = new Intent(Getting_Around_Activity.this,
+                        BusActivity.class);
+                startActivity(myIntent);
+                break;
+            }
+
+
+        }
     }
 }
